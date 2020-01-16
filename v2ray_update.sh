cff5fcbcb50c2b3919f473cc565275b389a7bd8d
Lenyu2020 <59961153+Lenyu2020@users.noreply.github.com>
2020-01-16 19:54:16 +0800

    Create v2ray_update.sh

---
+++ v2ray_update.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#只能x86-64的openwrt系统上，更新lean大佬的XXXPLUS的v2插件！
+#用SSH终端登录路由后台，把这段代码用:vi v2rayupdate.sh 然后:bash  v2rayupdate.sh 运行即可
+#By xxx
+	cd /tmp/tmp
+	echo
+	 echo -e " $green 正在获取网络最新版信息..$none"
+	 echo
+	 echo -e " $green 只能x86-64的openwrt系统上，更新lean大佬的XXXPLUS的v2插件！$none"
+	v2ray_latest_ver="$(curl -H 'Cache-Control: no-cache' -s https://api.github.com/repos/v2ray/v2ray-core/releases/latest | grep 'tag_name' | cut -d\" -f4)"
+	sleep 3
+	v2ray_ver=v`/usr/bin/v2ray/v2ray -version | grep "V2" | awk '{print $2}'`  
+	#echo $v2ray_ver
+	echo
+	if [ $v2ray_ver != $v2ray_latest_ver ]; then
+		echo
+		echo -e " $green 咦...发现新版本耶....正在拼命更新.......$none"
+		echo
+		v2ray_download_link="https://github.com/v2ray/v2ray-core/releases/download/$v2ray_latest_ver/v2ray-linux-64.zip"
+		echo
+		echo
+		wget  "$v2ray_download_link"
+		unzip -o v2ray-linux-64.zip &> /dev/null
+		echo
+	else
+		echo
+		echo -e " $green 木有发现新版本....$none"
+		echo
+		exit 0
+	fi
+    if [ -e /tmp/tmp/v2ray ]; then
+		mv -f /tmp/tmp/v2ctl  /usr/bin/v2ray
+		mv -f /tmp/tmp/v2ray  /usr/bin/v2ray
+		chmod 755 /usr/bin/v2ray/v2ctl
+		chmod 755 /usr/bin/v2ray/v2ray
+		/etc/init.d/shadowsocksr restart
+		#/etc/init.d/passwall restart
+		echo
+        echo -e " $green 更新成功啦...当前 V2Ray 版本: ${cyan}$v2ray_latest_ver$none"
+		echo
+		exit 0
+    else
+        echo -e "
+        $red 下载 V2Ray 失败啦..可能是你的 VPS 网络太辣鸡了...请重试...$none
+        " && exit 0
+    fi
+
+
+
+
