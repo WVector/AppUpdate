cb5cc95601ceae1d151a2ebb56b87e24570fc60d
Lenyu2020 <59961153+Lenyu2020@users.noreply.github.com>
2020-01-17 04:45:09 +0800

    Update v2ray_update.sh

--- v2ray_update.sh
+++ v2ray_update.sh
@@ -27,8 +27,9 @@
 	 echo
 	 echo -e " $green 2.只能x86-64的openwrt系统上，更新lean大佬的XXXPLUS的v2插件！$none"
 	v2ray_latest_ver="$(curl -H 'Cache-Control: no-cache' -s https://api.github.com/repos/v2ray/v2ray-core/releases/latest | grep 'tag_name' | cut -d\" -f4)"
-	sleep 3 &> /dev/null
-	if [ 0$v2ray_ver != 0$v2ray_latest_ver ]; then
+	sleep 2
+	if [ $v2ray_ver != $v2ray_latest_ver ]; then
+		clear
 		echo
 		echo -e " $green 咦...发现新版本耶....正在拼命更新.......$none"
 		echo
@@ -39,6 +40,7 @@
 		unzip -o v2ray-linux-64.zip &> /dev/null
 		echo
 	else
+		clear
 		echo
 		echo -e " $green 木有发现新版本....$none"
 		echo
