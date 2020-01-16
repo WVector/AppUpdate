e1f18d28e53e611c6139bf06cd511037e520ac18
Lenyu2020 <59961153+Lenyu2020@users.noreply.github.com>
2020-01-16 20:18:41 +0800

    Update v2ray_update.sh

--- v2ray_update.sh
+++ v2ray_update.sh
@@ -9,12 +9,14 @@
 	 echo
 	 echo -e " $green 只能x86-64的openwrt系统上，更新lean大佬的XXXPLUS的v2插件！$none"
 	v2ray_latest_ver="$(curl -H 'Cache-Control: no-cache' -s https://api.github.com/repos/v2ray/v2ray-core/releases/latest | grep 'tag_name' | cut -d\" -f4)"
-	sleep 3
 	if [ -n "$v2ray_latest_ver"]; then
+    echo "连接GitHub失败，请科学后再试试！"
+	fi
+	sleep 3
+	v2ray_ver=v`/usr/bin/v2ray/v2ray -version | grep "V2" | awk '{print $2}'`  
+	if [ -n "$v2ray_ver"]; then
     echo "请先启动lean大佬的SSRPULS+，并选择v2ray方式启动后再试！"
 	fi
-	v2ray_ver=v`/usr/bin/v2ray/v2ray -version | grep "V2" | awk '{print $2}'`  
-	#echo $v2ray_ver
 	echo
 	if [ $v2ray_ver != $v2ray_latest_ver ]; then
 		echo
@@ -52,4 +54,9 @@
         $red 下载 V2Ray 失败啦..可能是你的 VPS 网络太辣鸡了...请重试...$none
         " && exit 0
     fi
+
+
+
+
+
 
