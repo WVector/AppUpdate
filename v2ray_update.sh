dba645324eff126a0e500e4faf23cf4d13c4e34c
Lenyu2020 <59961153+Lenyu2020@users.noreply.github.com>
2020-01-17 04:39:38 +0800

    Update v2ray_update.sh

--- v2ray_update.sh
+++ v2ray_update.sh
@@ -22,16 +22,13 @@
 		echo
 		echo -e " $green 请先启动lean大佬的SSRPULS+，并选择v2ray方式启动后再试！$none"
 		exit 0
-		echo
-		echo
 	 fi
-	 echo
 	 echo -e " $green 1.正在获取网络最新版信息..$none"
 	 echo
 	 echo -e " $green 2.只能x86-64的openwrt系统上，更新lean大佬的XXXPLUS的v2插件！$none"
 	v2ray_latest_ver="$(curl -H 'Cache-Control: no-cache' -s https://api.github.com/repos/v2ray/v2ray-core/releases/latest | grep 'tag_name' | cut -d\" -f4)"
 	sleep 3
-	if [ $v2ray_ver != $v2ray_latest_ver ]; then
+	if [ 0$v2ray_ver != 0$v2ray_latest_ver ]; then
 		echo
 		echo -e " $green 咦...发现新版本耶....正在拼命更新.......$none"
 		echo
