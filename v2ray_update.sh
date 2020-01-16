6d8b887d3d7aa8fd424ed3fc692ece141d635bee
Lenyu2020 <59961153+Lenyu2020@users.noreply.github.com>
2020-01-16 20:04:40 +0800

    Update v2ray_update.sh

--- v2ray_update.sh
+++ v2ray_update.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #只能x86-64的openwrt系统上，更新lean大佬的XXXPLUS的v2插件！
 #用SSH终端登录路由后台，把这段代码用:vi v2rayupdate.sh 然后:bash  v2rayupdate.sh 运行即可
+#	bash -c "$(wget -O- https://git.io/JvUxL)"
 #By xxx
 	cd /tmp/tmp
 	echo
@@ -9,6 +10,8 @@
 	 echo -e " $green 只能x86-64的openwrt系统上，更新lean大佬的XXXPLUS的v2插件！$none"
 	v2ray_latest_ver="$(curl -H 'Cache-Control: no-cache' -s https://api.github.com/repos/v2ray/v2ray-core/releases/latest | grep 'tag_name' | cut -d\" -f4)"
 	sleep 3
+	#echo $v2ray_latest_ver
+	##显示openwrt的v2ray版本号命令：
 	v2ray_ver=v`/usr/bin/v2ray/v2ray -version | grep "V2" | awk '{print $2}'`  
 	#echo $v2ray_ver
 	echo
@@ -38,13 +41,16 @@
 		echo
         echo -e " $green 更新成功啦...当前 V2Ray 版本: ${cyan}$v2ray_latest_ver$none"
 		echo
+		echo -e " $yellow 温馨提示: 为了避免出现莫名其妙的问题...V2Ray 客户端的版本最好和服务器的版本保持一致$none"
+		echo
+		echo  -e " $yellow 请到路由后台程序保存应用一下XXSSRplus+，确保成功应用！$none"
+		echo
 		exit 0
     else
         echo -e "
         $red 下载 V2Ray 失败啦..可能是你的 VPS 网络太辣鸡了...请重试...$none
         " && exit 0
     fi
-
 
 
 
