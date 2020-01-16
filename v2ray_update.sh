d794d80ae5362707e8cc0d9a6c991e6734f88db2
Lenyu2020 <59961153+Lenyu2020@users.noreply.github.com>
2020-01-17 04:36:52 +0800

    Update v2ray_update.sh

--- v2ray_update.sh
+++ v2ray_update.sh
@@ -8,9 +8,8 @@
 	 clear
 	 echo
 	 echo 
-	 st=`ps | grep "passwall" | awk '{print $8}'`  &> /dev/null
-	 sleep 1
-	 if [ `echo ${st: 10:8}` =  passwall ]; then
+	 st=0`ps | grep "passwall" | awk '{print $8}'`  &> /dev/null
+	 if [ 0`echo ${st: 10:8}` =  0passwall ]; then
 		echo -e " 请先关闭passwall并启动lean大佬的SSRPULS+，并选择v2ray方式启动再试!" 
 		echo
 		exit 0
@@ -32,12 +31,6 @@
 	 echo -e " $green 2.只能x86-64的openwrt系统上，更新lean大佬的XXXPLUS的v2插件！$none"
 	v2ray_latest_ver="$(curl -H 'Cache-Control: no-cache' -s https://api.github.com/repos/v2ray/v2ray-core/releases/latest | grep 'tag_name' | cut -d\" -f4)"
 	sleep 3
-	if [ -n "$v2ray_latest_ver" ]; then
-		echo
-		#echo "连接GitHub失败，请科学后再试试！"
-		#exit 0
-	fi
-	echo
 	if [ $v2ray_ver != $v2ray_latest_ver ]; then
 		echo
 		echo -e " $green 咦...发现新版本耶....正在拼命更新.......$none"
