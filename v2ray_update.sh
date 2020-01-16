38c01bfd269a1afa065ff2284bd0a7c44b0ad271
Lenyu2020 <59961153+Lenyu2020@users.noreply.github.com>
2020-01-16 21:00:33 +0800

    Update v2ray_update.sh

--- v2ray_update.sh
+++ v2ray_update.sh
@@ -8,21 +8,25 @@
 	 echo 
 	 st=`ps | grep "passwall" | awk '{print $8}'` 
 	 if [ `echo ${st: 10:8}` =  passwall ]; then
+		clear
 		echo -e " 请先关闭passwall并启动lean大佬的SSRPULS+，并选择v2ray方式启动再试!" 
 		exit 0
 	 fi
 	 echo
+	 clear
 	 echo -e " $green 正在获取网络最新版信息..$none"
 	 echo
 	 echo -e " $green 只能x86-64的openwrt系统上，更新lean大佬的XXXPLUS的v2插件！$none"
 	v2ray_latest_ver="$(curl -H 'Cache-Control: no-cache' -s https://api.github.com/repos/v2ray/v2ray-core/releases/latest | grep 'tag_name' | cut -d\" -f4)"
 	sleep 3
 	if [ -n "$v2ray_latest_ver" ]; then
+		clear
 		echo "连接GitHub失败，请科学后再试试！"
 		exit 0
 	fi
 	v2ray_ver=v`/usr/bin/v2ray/v2ray -version | grep "V2" | awk '{print $2}'`  
 	if [ "$v2ray_ver" = v ]; then
+		clear
 		echo "请先启动lean大佬的SSRPULS+，并选择v2ray方式启动后再试！"
 		exit 0
 	fi
@@ -49,7 +53,7 @@
 		chmod 755 /usr/bin/v2ray/v2ctl
 		chmod 755 /usr/bin/v2ray/v2ray
 		/etc/init.d/shadowsocksr restart
-		#/etc/init.d/passwall restart
+		clear
 		echo
         echo -e " $green 更新成功啦...当前 V2Ray 版本: ${cyan}$v2ray_latest_ver$none"
 		echo
@@ -59,6 +63,7 @@
 		echo
 		exit 0
     else
+		clear
         echo -e "
         $red 下载 V2Ray 失败啦..可能是你的 VPS 网络太辣鸡了...请重试...$none
         " && exit 0
