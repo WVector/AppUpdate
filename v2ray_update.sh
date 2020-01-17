549ea236c2234df297236d443d3d437b52e58d7d
Lenyu2020 <59961153+Lenyu2020@users.noreply.github.com>
2020-01-17 17:52:11 +0800

    Update v2ray_update.sh

--- v2ray_update.sh
+++ v2ray_update.sh
@@ -8,15 +8,16 @@
 	 clear
 	 echo
 	 echo 
-	 st=0`ps | grep "passwall" | awk '{print $8}'`  &> /dev/null
-	 if [ 0`echo ${st: 10:8}` =  0passwall ]; then
+	 st="`ps | grep "passwall" | awk '{print $8}'`"  &> /dev/null
+	 if [ "`echo ${st: 10:8}`" =  "passwall" ]; then
 		echo -e " 请先关闭passwall并启动lean大佬的SSRPULS+，并选择v2ray方式启动再试!" 
 		echo
 		exit 0
 	 fi
 	 echo
-	 sr=0`ps -ef | grep "/var/etc/v2-ssr" | awk '{print $7}'` &> /dev/null
-	 if [ $sr != "0/var/etc/v2-ssr-retcp.json" ]; then
+	 sr="`ps -ef | grep "/var/etc/v2-ssr" | awk '{print $7}'`" &> /dev/null
+	 #sr="$(ps -ef | grep "/var/etc/v2-ssr" | awk '{print $7}')"
+	 if [ $sr != "/var/etc/v2-ssr-retcp.json" ]; then
 		clear
 		echo
 		echo -e " $green 请先启动lean大佬的SSRPULS+，并选择v2ray方式启动后再试！$none"
@@ -25,9 +26,11 @@
 	 echo -e " $green 1.正在获取网络最新版信息..$none"
 	 echo
 	 echo -e " $green 2.只能x86-64的openwrt系统上，更新lean大佬的XXXPLUS的v2插件！$none"
+	#curl -s https://api.github.com/repos/v2ray/v2ray-core/releases/latest  | grep "tag_name" | cut -d\" -f4
 	v2ray_latest_ver="$(curl -H 'Cache-Control: no-cache' -s https://api.github.com/repos/v2ray/v2ray-core/releases/latest | grep 'tag_name' | cut -d\" -f4)" && sleep 2
-	v2ray_ver=v`/usr/bin/v2ray/v2ray -version | grep "V2" | awk '{print $2}'`   &> /dev/null
-	if [ $v2ray_ver != $v2ray_latest_ver ]; then
+	v2ray_ver="v`/usr/bin/v2ray/v2ray -version | grep "V2" | awk '{print $2}'`"   &> /dev/null 
+	#v2ray_ver="v$(/usr/bin/v2ray/v2ray -version | grep "V2" | awk '{print $2}')" &> /dev/null 
+	if [ "$v2ray_ver" != "$v2ray_latest_ver" ]; then
 		clear
 		echo
 		echo -e " $green 咦...发现新版本耶....正在拼命更新.......$none"
@@ -54,7 +57,7 @@
 		echo
         	echo -e " $green 更新成功啦...当前 V2Ray 版本: ${cyan}$v2ray_latest_ver$none"
 		echo
-		echo -e " $yellow 温馨提示: 为了避免出现莫名其妙的问题...V2Ray 服务端的版本最好和服务器的版本保持一致$none"
+		echo -e " $yellow 温馨提示: 为了避免出现莫名其妙的问题...V2Ray 客户端的版本最好和服务器的版本保持一致$none"
 		echo
 		echo
 		exit 0
