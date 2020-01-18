557bc30ca7bbfd811da2a0f165bf4e705c6ff4f3
Lenyu2020 <59961153+Lenyu2020@users.noreply.github.com>
2020-01-18 15:00:37 +0800

    Update v2_update.sh

--- v2_update.sh
+++ v2_update.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-#只能x86-64的openwrt系统上，能lean大佬的XXXPLUS和passwall的v2插件
+#只能在x86-64的openwrt系统上更lean大佬的XXXPLUS和passwall的v2插件！
 #	bash -c "$(wget -O- https://git.io/JvTVT)"
-#Version: v2.0.1 By Len yu.
+#Version: v2.0.2 By Len yu.
 cd /tmp/tmp
 rm -rf *
 echo
 echo -e " $green Version: v2.0.1 By Len yu.$none"
 echo
-echo -e " $green 只能x86-64的openwrt系统上，可更新lean大佬的XXXPLUS和passwall的v2插件！$none"
+echo -e " $green 2.只能x86-64的openwrt系统上，可更新lean大佬的XXXPLUS和passwall的v2插件！$none"
 sleep 2
 clear
 echo 
-echo -e " $green 正在获取网络v2ray最新版信息..$none"
+echo -e " $green 1.正在获取网络v2ray最新版信息..$none"
 echo
 #judgment
 v2ray_latest_ver="$(curl -H 'Cache-Control: no-cache' -s https://api.github.com/repos/v2ray/v2ray-core/releases/latest | grep 'tag_name' | cut -d\" -f4)" 
@@ -37,7 +37,7 @@ else
 fi
 ##install
 st="`ps | grep "passwall" | awk '{print $8}'`"  &> /dev/null
-sr="$(ps -ef | grep "/var/etc/v2-ssr" | awk '{print $7}')" &> /dev/null
+sr="`ps -ef | grep "ssr-retcp" | awk '{print $9}' | echo ${sr: 13:5}`" &> /dev/null
 if [[ "`echo ${st: 10:8}`" =  "passwall" &&  -e /tmp/tmp/v2ray ]]; then
 		mv -f /tmp/tmp/v2ctl  /usr/bin/v2ray
 		mv -f /tmp/tmp/v2ray  /usr/bin/v2ray
@@ -45,20 +45,20 @@ if [[ "`echo ${st: 10:8}`" =  "passwall" &&  -e /tmp/tmp/v2ray ]]; then
 		chmod 755 /usr/bin/v2ray/v2ray
 		/etc/init.d/passwall restart
 		echo
-        	echo -e " $green 更新成功啦...当前 V2Ray 版本: ${cyan}$v2ray_latest_ver$none"
+        echo -e " $green 更新成功啦...当前 V2Ray 版本: ${cyan}$v2ray_latest_ver$none"
 		echo
-		echo -e " $yellow 温馨提示: 为了避免出现莫名其妙的问题...V2Ray路由端的版本最好和服务器的版本保持一致$none"
+		echo -e " $yellow 温馨提示: 为了避免出现莫名其妙的问题..V2Ray路由端的版本最好和服务器的版本保持一致$none"
 		echo
 		echo
 		exit 0
-elif [[ "$sr" = "/var/etc/v2-ssr-retcp.json" &&  -e /tmp/tmp/v2ray ]]; then
+elif [[ "$sr" = "retcp" &&  -e /tmp/tmp/v2ray ]]; then
 		mv -f /tmp/tmp/v2ctl  /usr/bin/v2ray
 		mv -f /tmp/tmp/v2ray  /usr/bin/v2ray
 		chmod 755 /usr/bin/v2ray/v2ctl
 		chmod 755 /usr/bin/v2ray/v2ray
 		/etc/init.d/shadowsocksr restart
 		echo
-        	echo -e " $green 更新成功啦...当前 V2Ray 版本: ${cyan}$v2ray_latest_ver$none"
+        echo -e " $green 更新成功啦...当前 V2Ray 版本: ${cyan}$v2ray_latest_ver$none"
 		echo
 		echo -e " $yellow 温馨提示: 为了避免出现莫名其妙的问题...V2Ray路由端的版本最好和服务器的版本保持一致$none"
 		echo
@@ -66,6 +66,7 @@ elif [[ "$sr" = "/var/etc/v2-ssr-retcp.json" &&  -e /tmp/tmp/v2ray ]]; then
 		exit 0
 else
 		echo
-        	echo -e "$red 下载 V2Ray 失败啦..可能是你的 VPS 网络太辣鸡了...请重试...$none"
-        	exit 0
+        echo -e "$red 下载 V2Ray 失败[需要连接科学]...请重试...$none"
+        exit 0
 fi
+
