a59fa0dcc04d8a1c33c083a5c3c7091fa3b42f0d
Lenyu2020 <59961153+Lenyu2020@users.noreply.github.com>
2021-01-19 23:33:33 +0800

    Update xray-update.sh
    1.增加passwall更新与否判断；
    2.逻辑细节优化；
    3.提升版本至version v1.1.3。

--- xray-update.sh
+++ xray-update.sh
@@ -1,6 +1,6 @@
 #/usr/bin/bash
 #Author Lenyu 
-#version v1.1.2
+#version v1.1.3
 #获取当前绝对路径
 #echo "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" 
 cd /home/lenyu
@@ -69,6 +69,34 @@ fi
 echo
 ###判断ssr+是否有更新-结束###
 echo
+###判断passwall是否有更新-开始###
+new_passw=$(wget --no-check-certificate -qO- -t1 -T3 "https://github.com/xiaorouji/openwrt-passwall/commits/main")
+sleep 0.2
+echo $new_passw > passw1
+printf '%s\t %s\t %s\t %s\n' $(cat passw1) > passw2
+grep "https://github.com/xiaorouji/openwrt-passwall/commit/" passw2 > passw3
+sleep 0.1
+#获得最新值
+new_passw=` cat passw3 | cut -b 70-109`
+echo $new_passw > new_passw
+#if条件判断old_ssr文件是否存在，不存在创建 -f 判断文件 -d 判断文件夹
+if [ ! -f "old_passw" ]; then
+  clear	
+  echo "old_passw被删除正在创建！"
+  sleep 0.1
+  echo $new_passw > old_passw
+fi
+sleep 0.1 
+old_passw=`cat old_passw`
+if [ "$new_passw" = "$old_passw" ]; then
+	echo "no_update" > /home/lenyu/nopassw
+else
+	echo "update" > /home/lenyu/nopassw
+	echo $new_passw > old_passw
+fi
+echo
+###判断passwall是否有更新-结束###
+echo
 #检查固件是否更新
 cd /home/lenyu/lede
 clear
@@ -94,15 +122,15 @@ echo "update" > /home/lenyu/noopclash
 fi
 echo
 clear
-	sleep 0.1
-	echo "#请检查脚本可用性，否则按任意键,开始编译固件…"
-#	read
+sleep 0.1
+#总结判断之
 nolede=`cat /home/lenyu/nolede`
 noopclash=`cat /home/lenyu/noopclash`
 noxray=`cat /home/lenyu/noxray`
 nossr=`cat /home/lenyu/nossr`
+nopassw=`cat /home/lenyu/nopassw`
 sleep 0.2
-if [[("$nolede" = "update") || ("$noopclash" = "update") || ("$noxray" = "update") || ("$nossr" = "update" )]]; then
+if [[("$nolede" = "update") || ("$noopclash" = "update") || ("$noxray" = "update") || ("$nossr" = "update" ) || ("$nopassw"  = "update" )]]; then
 	clear
 	echo
 	echo "发现更新，请稍后…"
@@ -116,12 +144,13 @@ if [[("$nolede" = "update") || ("$noopclash" = "update") || ("$noxray" = "update
 	rm -rf /home/lenyu/noopclash
 	rm -rf /home/lenyu/nolede
 	rm -rf /home/lenyu/nossr
+	rm -rf /home/lenyu/nopassw
 	echo "固件编译成功，脚本退出！"
 	echo
 	exit 0
 fi
 echo
-if [[("$nolede" = "no_update") && ("$noopclash" = "no_update") && ("$noxray" = "no_update") && ("$nossr" = "no_update" )]]; then
+if [[("$nolede" = "no_update") && ("$noopclash" = "no_update") && ("$noxray" = "no_update") && ("$nossr" = "no_update" ) && ("$nopassw"  = "no_update" )]]; then
 	clear
 	echo
 	echo "呃呃…检查lede/ssr+/xray/passwall/openclash源码，没有一个源码更新哟…还是稍安勿躁…" 
@@ -132,6 +161,7 @@ rm -rf /home/lenyu/noxray
 rm -rf /home/lenyu/noopclash
 rm -rf /home/lenyu/nolede
 rm -rf /home/lenyu/nossr
+rm -rf /home/lenyu/nopassw
 echo
 echo "脚本退出！"
 echo
